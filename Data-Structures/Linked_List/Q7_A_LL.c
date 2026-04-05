324763642d22461ca143358bf9be7c96b6fc09ee
HyunSeongLee <devhyun05@gmail.com>
2026-04-06 03:05:14 +0900

    Q7_A_LL 해결

--- Data-Structures/Linked_List/Q7_A_LL.c
+++ Data-Structures/Linked_List/Q7_A_LL.c
@@ -87,7 +87,19 @@ int main()
 
 void RecursiveReverse(ListNode **ptrHead)
 {
-	/* add your code here */
+	ListNode *first, *rest; 
+	first = *ptrHead; 
+	rest = (*ptrHead)->next; 
+
+	if (rest == NULL)
+	{
+		return;
+	}
+	RecursiveReverse(&rest); 
+	first->next->next = first; 
+	first->next = NULL; 
+	*ptrHead = rest; 
+	
 }
 
 //////////////////////////////////////////////////////////////////////////////////
